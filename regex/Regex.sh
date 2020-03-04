a7082f2d5aecbf7ca725a2c7dde1fd8c39a64eff
vampire16 <siddheshsalvi525@gmai.com>
2020-03-04 12:50:51 +0530

    ADDED : Valid last name

--- regex/Regex.sh
+++ regex/Regex.sh
@@ -2,12 +2,28 @@
 
 echo "Welcome"
 
-pattern="^[A-Z][a-zA-Z]{2,}$"
-read -p "Enter name : " name
+function firstName(){
+	pattern="^[A-Z][a-zA-Z]{2,}$"
+	read -p "Enter name : " name
 
-if [[ $name =~ $pattern ]]
-then
-	echo "Valid"
-else
-	echo "Invalid"
-fi
+	if [[ $name =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+
+function lastName(){
+	pattern="^[A-Z][a-zA-Z]{2,}$"
+	read -p "Enter last name : " lastName
+	if [[ $lastName =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+
+firstName
+lastName
